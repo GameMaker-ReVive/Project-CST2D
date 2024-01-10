8bd3349337baed1470736c3cba8ef9b7fa114b9b
Peinoi <124995489+Peinoi@users.noreply.github.com>
2024-01-10 23:47:58 +0900

    카드 뽑기

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     public GameObject spawn_Point;
     public GameObject block;
+    public GameObject[] card;
+
+    void Awake(){
+        instance =this;
+        StartCard();
+    }
+
     void Start()
     {
         
@@ -19,5 +27,21 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             Instantiate(block, spawn_Point.transform.position, spawn_Point.transform.rotation);
         }
+    }
+    public void StartCard(){
+
+
+        for(int index=0; index<4; index++){
+            int ran = Random.Range(0,card.Length);
+            while(card[ran].activeSelf){
+                ran = Random.Range(0,card.Length);
+                if(!card[ran].activeSelf){
+                    card[ran].SetActive(true);
+                    break;
+                }
+            }
+            card[ran].SetActive(true);
+        }
+
     }
 }
