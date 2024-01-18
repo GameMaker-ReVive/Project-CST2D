307d78e0aa5512ad5315630482894336087e667a
Peinoi <124995489+Peinoi@users.noreply.github.com>
2024-01-18 12:16:54 +0900

    afadf

--- Assets/Script/CardManager.cs
+++ Assets/Script/CardManager.cs
@@ -4,39 +4,19 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class CardManager : MonoBehaviour
 {
-    Vector3 pos;
-    void Awake(){
-        pos = this.transform.position;
-    }
-    void Update(){
-        //CusorY();
-    }
+    
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
+    private void OnMouseEnter() {
         Skill.instance.SkillStay();
         Debug.Log("Stay");
     }
-    public void OnPointerExit(PointerEventData eventData)
-    {
+    private void OnMouseExit() {
         Skill.instance.SkillGo();
         Debug.Log("Go");
     }
 
-    void CusorY(){
-        if(Skill.instance.skillCnt == 0)
-            return;
+    
 
-        if(pos.y >=450 || pos.y <= -370){
-            //Skill.instance.SkillStay();
-            Skill.instance.skillStop = false;
-            Debug.Log("Stay");
-        }
-        else if(pos.y <450 || pos.y > -370){
-            //Skill.instance.SkillGo();   
-            Skill.instance.skillStop = true;
-            Debug.Log("Go");
-        }
-    }
+    
    
 }
